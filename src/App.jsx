805b776cae23d89b2799742f8663b844ac8800d6
Marcos Almeida Koch <115181147+Marcos0098@users.noreply.github.com>
2022-12-01 19:12:15 -0300

    transferencia de projeto
    .

---
+++ src/App.jsx
@@ -0,0 +1,140 @@
+import { useState } from 'react';
+import { itemsData } from "./itemsData";
+
+import kingdom from "./items-image/Kingdom.png";
+import paypal from "./items-image/Paypal.png"
+
+import './App.css';
+
+function App() {
+  const [tentativas, setTentativas] = useState(0);
+  const [cristalNegro, setCristalNegro] = useState(0);
+  const [territe, setTerrite] = useState(0);
+  const [stones, setStones] = useState(0);
+  const [weapon, setWeapon] = useState();
+  const [trys, setTrys] = useState(20);
+  const [success, setSuccess] = useState(0);
+  const [failure, setFailure] = useState(0);
+  const [active, setMode] = useState(false);
+  const [math, setMath] = useState(0)
+  const [resultado, setResultado] = useState("")
+  const [baseStat, setBaseStat] = useState(20)
+
+  const ToggleMode = () => {
+    setMode(!active);
+  };
+
+
+  const handleChange = (newWeapon) =>{
+    setWeapon(newWeapon);
+  }
+
+  function aprimoramento() {
+    setMath(Math.random())
+    setTentativas(tentativas + 1);
+    setCristalNegro(cristalNegro + 30);
+    setTerrite(territe + 100);
+    setStones(stones + 1);
+
+    if(math > 0.5){
+      setResultado("Sucesso")
+      setTrys((p) => (p - 1));
+      setTrys((p) => (p + 2));
+      setSuccess((p) => (p + 1));
+    }
+    if(math < 0.5){
+      setResultado("Falha")
+      setFailure((p) => (p + 1))
+      if(trys === 20){
+        setTrys(20)
+      }else{
+        setTrys((p) => (p) - 1);;
+      }
+    }
+  }
+
+
+
+  const refreshPage = () =>{
+    window.location.reload();
+  }
+
+  function initialEnhancement(e) {
+      e.preventDefault();
+      {baseStat === trys ? setBaseStat(baseStat) : setBaseStat(trys)};
+      ToggleMode();
+  }
+
+
+  return (
+    <div className="App">
+      <div className='container-main'>
+        <div className='container-item-selector'>
+          <h2>Select an item to enchant</h2>
+          <select onChange={(e) => handleChange(e.target.value)} value={weapon}>
+            <option selected disabled>Select your item</option>
+            {itemsData.name.map((item, index) => (<option key={item} className="itens" value={index}>{item}</option>))}
+          </select>
+
+          <div className={active ? "selecaoClose" : "selecaoOpen"}>
+          
+          <h4>Select the level of enhancement</h4>
+          <form onSubmit={initialEnhancement}>
+            <input type="number" min={20} max={99} value={trys} onChange={(e) => setTrys(e.target.value)}/>
+            <input type='submit'value="Enhance" ></input>
+          </form>
+          </div>
+
+          <button onClick={refreshPage}>Reset</button>
+        </div>
+
+        <div className='item-enchant'>
+          <div className='container-item-top'>
+            {weapon && <img src={itemsData.image[weapon]} alt="imgWeapon" />}
+            <h2>{resultado === "Sucesso" ?  <><h2 style={{color: "#9F4FF1"}}>Cursed</h2><h2>{itemsData.name[weapon]}</h2><h2 style={{color: "#9F4FF1"}}>{`+${trys}`}</h2></> : <><h2>{itemsData.name[weapon]}</h2><h2>{`+${trys}`}</h2></>}</h2>
+            <h4>Enhance: {success - failure > 0 ? `+${success - failure}` : success - failure}</h4>
+            <button onClick={aprimoramento}>Enhance Dark Magic (50% Success)</button>
+          </div>
+          <h3>Results</h3>
+          <div className='item-results'>
+            
+            <h4>Initial Enhancement</h4>
+            <p>{baseStat}</p>
+            <h4>Current Enhancement</h4>
+            <p style={{color:"#9F4FF1"}}>{trys}</p>
+            <h4>Enhancement Stones Used</h4>
+            <p>{stones}</p>
+            <h4>Territe Used</h4>
+            <p>{territe}</p>
+            <h4>Dark Crystals Used</h4>
+            <p>{cristalNegro}</p>
+            <h4>Attempts</h4>
+            <p>{tentativas}</p>
+            <h4>Success</h4>
+            <p style={{color:"#10B981"}}>{success}</p>
+            <h4>Failures</h4>
+            <p style={{color:"#EF4444"}}>{failure}</p>
+
+          </div>
+        </div>
+      </div>
+      <footer>
+        <div className='donation'>
+          <h2>Help me with any amount!</h2>
+          <a href=""><img src={paypal}atl="Donate Paypal" /></a>
+        </div>
+        <div className='specialThanks'>
+          <div className='kingdom'>
+            <h2>Kingdom</h2>
+            <img src={kingdom}/>
+            <p>DarkReunion</p>
+          </div>
+
+        </div>
+
+      </footer>
+    </div>
+  )
+}
+
+export default App
---
+++ src/itemsData.jsx
@@ -0,0 +1,76 @@
+import guardianSword from "./items-image/Guardians-blade.png";
+import calamitySword from "./items-image/Calamity-blade.png";
+import championsSword from "./items-image/Champions-blade.png";
+import tidalSword from "./items-image/Tidal-blade.png";
+import divineSword from "./items-image/Divine-blade.png";
+import infinitySword from "./items-image/Infinity-blade.png";
+import heroSword from "./items-image/Hero-blade.png";
+import chronosSword from "./items-image/Chronos-blade.png";
+import demonSword from "./items-image/Demon-blade.png";
+import milkyWaySword from "./items-image/Milky-way-blade.png";
+import decimationSword from "./items-image/Decimation-blade.png"
+
+import guardianRifle from "./items-image/Guardian-rifle.png";
+import calamityRifle from "./items-image/Calamity-rifle.png";
+import championsRifle from "./items-image/Champions-rifle.png";
+import tidalRifle from "./items-image/Tidal-rifle.png";
+import divineRifle from "./items-image/Divine-rifle.png";
+import infinityRifle from "./items-image/Infinity-rifle.png";
+import heroRifle from "./items-image/Hero-rifle.png";
+import chronosRifle from "./items-image/Chronos-rifle.png";
+import demonRifle from "./items-image/Demon-rifle.png";
+import milkyWayRifle from "./items-image/Milky-way-rifle.png";
+import decimationRifle from "./items-image/Decimation-rifle.png"
+
+import guardianSpear from "./items-image/Guardian-spear.png";
+import calamitySpear from "./items-image/Calamity-spear.png";
+import championsSpear from "./items-image/Champions-spear.png";
+import tidalSpear from "./items-image/Tidal-spear.png";
+import divineSpear from "./items-image/Divine-spear.png";
+import infinitySpear from "./items-image/Infinity-spear.png";
+import heroSpear from './items-image/Hero-spear.png';
+import chronosSpear from "./items-image/Chronos-spear.png";
+import demonSpear from "./items-image/Demon-spear.png";
+import milkyWaySpear from "./items-image/Milky-way-spear.png";
+import decimationSpear from "./items-image/Decimation-spear.png";
+
+import guardianHammer from "./items-image/Guardian-hammer.png";
+import calamityHammer from "./items-image/Calamity-hammer.png";
+import championsHammer from "./items-image/Champions-hammer.png";
+import tidalHammer from "./items-image/Tidal-hammer.png";
+import divineHammer from "./items-image/Divine-hammer.png";
+import infinityHammer from "./items-image/Infinity-hammer.png";
+import heroHammer from './items-image/Hero-hammer.png';
+import chronosHammer from "./items-image/Chronos-hammer.png";
+import demonHammer from "./items-image/Demon-hammer.png";
+import milkyWayHammer from "./items-image/Milky-way-hammer.png";
+import decimationHammer from "./items-image/Decimation-hammer.png";
+
+import guardianBow from "./items-image/Guardian-bow.png";
+import calamityBow from "./items-image/Calamity-bow.png";
+import championsBow from "./items-image/Champions-bow.png";
+import tidalBow from "./items-image/Tidal-bow.png";
+import divineBow from "./items-image/Divine-bow.png";
+import infinityBow from "./items-image/Infinity-bow.png";
+import heroBow from './items-image/Hero-bow.png';
+import chronosBow from "./items-image/Chronos-bow.png";
+import demonBow from "./items-image/Demon-bow.png";
+import milkyWayBow from "./items-image/Milky-way-bow.png";
+import decimationBow from "./items-image/Decimation-bow.png";
+
+
+export const itemsData = {
+
+      name: ["Guardian's Blade","Calamity Blade", "Champion's Blade", "Tidal Blade", "Divine Sword", "Infinity Blade", "Hero's Blade", "Chronos Blade", "Demon's Blade", "Milky Way Sword", "Decimation Blade",
+             "Guardian's Rifle", "Calamity Rifle", "Champion's Rifle", "Tidal Rifle", "Divine Rifle", "Infinity Rifle", "Hero's Rifle", "Chronos Rifle", "Demon's Rifle", "Milky Way Rifle", "Decimation Rifle",
+             "Guardian's Spear", "Calamity Spear", "Champion's Spear", "Tidal Spear", "Divine Spear", "Infinity Spear", "Hero's Spear", "Chronos Spear", "Demon's Spear", "Milky Way Spear", "Decimation Spear",
+             "Guardian's Hammer", "Calamity Hammer", "Champion's Hammer", "Tidal Hammer", "Divine Hammer", "Infinity Hammer", "Hero's Hammer", "Chronos Hammer", "Demon's Hammer", "Milky Way Hammer", "Decimation Hammer",
+             "Guardian's Bow", "Calamity Bow", "Champion's Bow", "Tidal Bow", "Divine Bow", "Infinity Bow", "Hero's Bow", "Chronos Bow", "Demon's Bow", "Milky Way Bow", "Decimation Bow"],
+    
+      image: [guardianSword, calamitySword, championsSword, tidalSword, divineSword, infinitySword, heroSword, chronosSword, demonSword, milkyWaySword, decimationSword,
+              guardianRifle, calamityRifle, championsRifle, tidalRifle, divineRifle, infinityRifle, heroRifle, chronosRifle, demonRifle, milkyWayRifle, decimationRifle,
+              guardianSpear, calamitySpear, championsSpear, tidalSpear, divineSpear, infinitySpear, heroSpear, chronosSpear, demonSpear, milkyWaySpear, decimationSpear,
+              guardianHammer, calamityHammer, championsHammer, tidalHammer, divineHammer, infinityHammer, heroHammer, chronosHammer, demonHammer, milkyWayHammer, decimationHammer,
+              guardianBow, calamityBow, championsBow, tidalBow, divineBow, infinityBow, heroBow, chronosBow, demonBow, milkyWayBow, decimationBow]
+  }
+  
